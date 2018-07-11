0d38eef226f056d424f5c1e2b1f6b46ba0ad8c70
Carlos Ferreira <conta@especializati.com.br>
2018-07-11 15:21:16 -0300

    JS na prática

---
+++ app.js
@@ -0,0 +1,55 @@
+//************************************************************
+//********************** Exemplo 01 **************************
+//****** Primeiro Programa
+// console.log('Hello World');
+
+
+
+//************************************************************
+//********************** Exemplo 02 **************************
+//****** Comentários
+// console.log('Hello World');
+/*
+console.log('Hello World');
+console.log('Hello World');
+*/
+
+
+
+//************************************************************
+//********************** Exemplo 03 **************************
+//****** Variáveis
+/*
+var carlos = 'Carlos Ferreira 2';
+console.log(carlos);
+
+carlos = true;
+console.log(carlos);
+
+carlos = 19999;
+console.log(carlos);
+
+carlos = 3.14;
+console.log(carlos);
+*/
+
+
+
+//************************************************************
+//********************** Exemplo 04 **************************
+//****** Concatenação
+/*
+var name = prompt('O seu nome?');
+var age = prompt('A sua idade?');
+var ageTwo = 90;
+
+console.log('O ' + name + ' tem ' + age + ' anos de idade.');
+console.log(age + '' + ageTwo);
+console.log(name + ' ' + age);
+*/
+
+
+
+//************************************************************
+//********************** Exemplo 04 **************************
+//****** Operadores Aritméticos
